e23ac1fe31f74d1c1556e4e40f33ffc5467a949b
tkaria <tkaria.info@gmail.com>
2020-06-10 03:49:49 +0900

    add project

---
+++ Editor/FindReferenceInScene.cs
@@ -0,0 +1,108 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.Reflection;
+using UnityEngine;
+using UnityEditor;
+
+
+namespace FindReferenceInScene
+{
+	
+	/// <summary>
+	///  GameObjectのヒエラルキー参照検索.
+	/// </summary>
+	public class HierarchyObjectFinder
+	{
+
+		[MenuItem("GameObject/Find Reference In Scene (Instanced)", false, 20)]
+		public static void FindReferenceInScene( )
+		{
+			HierarchyControl.FindReference(Selection.activeGameObject.GetInstanceID());
+		}
+		
+	}
+
+	/// <summary>
+	///  Componentのヒエラルキー参照検索.
+	/// </summary>
+	public static class HierarchyComponentFinder
+	{
+
+		[MenuItem("CONTEXT/Component/Find References In Scene (Instanced)")]
+		private static void FindReferencesInScene(MenuCommand command)
+		{
+			var targetComponent = command.context as Component;
+			if (targetComponent == null)
+			{
+				return;
+			}
+			HierarchyControl.FindReference(targetComponent.GetInstanceID());
+		}
+
+	}
+
+	/// <summary>
+	///  ヒエラルキー制御を行います.
+	/// </summary>
+	public static class HierarchyControl
+	{
+		#region Const
+
+		private const string hierarchyTypeName = "UnityEditor.SceneHierarchyWindow,UnityEditor";
+
+		#endregion // Const End.
+
+		#region Utils
+
+		private static string MakeSearchFilter(int instanceId)
+		{
+			return string.Format("ref:{0}:", instanceId);
+		}
+
+		public static SearchableEditorWindow GetHierarchyFilter()
+		{
+			var sceneHierarchyWindowType = Type.GetType(hierarchyTypeName);
+			return EditorWindow.GetWindow(sceneHierarchyWindowType) as SearchableEditorWindow;
+		}
+
+		public static void FindReference(int instanceId )
+		{
+			var hierarchyFilter = GetHierarchyFilter(); 
+			Reflection.SetSearchFilter( hierarchyFilter, MakeSearchFilter(instanceId), SearchableEditorWindow.SearchMode.All);
+			hierarchyFilter.Repaint();
+		}
+
+		#endregion // Utils End.
+
+	}
+
+	public static class Reflection
+	{
+
+		/// <summary>
+		///  ヒエラルキー内にフィルター検索を掛けます.
+		/// </summary>
+		public static void SetSearchFilter(SearchableEditorWindow hierarchyFilter, string searchFilter, SearchableEditorWindow.SearchMode searchMode)
+		{
+			const string reflectionMethodName = "SetSearchFilter";
+
+			var setSearchFilterMethod = hierarchyFilter.GetType().GetMethod(reflectionMethodName, BindingFlags.Instance | BindingFlags.NonPublic);
+
+		#if UNITY_2018_2_3
+			setSearchFilterMethod.Invoke(hierarchyFilter, new object[] { searchFilter, searchMode, false });
+		#elif UNITY_2018_2_21
+			setSearchFilterMethod.Invoke(hierarchyFilter, new object[] { searchFilter, searchMode, false, false });
+		#elif UNITY_2019_1_14
+			setSearchFilterMethod.Invoke(hierarchyFilter, new object[] { searchFilter, searchMode, false, false });
+		#else // UNITY_2018_3
+			setSearchFilterMethod.Invoke(hierarchyFilter, new object[] { searchFilter, searchMode });
+		#endif // UNITY VERSION.
+
+		}
+
+	}
+
+}
+
+#endif // UNITY_EDITOR End.
+
--- README.md
+++ README.md
@@ -1,2 +1,93 @@
 # FindReferenceInScene
-Component,GameObjectのヒエラルキー内参照検索
+
+[![License](https://img.shields.io/badge/license-MIT-green)](LICENSE)
+[![Build Status](https://img.shields.io/badge/build-passing-brightgreen)](#)
+[![NuGet](https://img.shields.io/badge/nuget-v1.0.0-blue)](#)
+
+Component, reference search within the hierarchy of GameObject.
+
+<!-- Badges: https://shields.io/ -->
+<!-- Reference -->
+<!-- https://kakakakakku.hatenablog.com/entry/2018/08/08/200903 -->
+
+<!-- Code Quality: https://app.codacy.com/ -->
+<!-- https://srz-zumix.blogspot.com/2018/07/cireview-codacy.html-->
+
+<!-- OTHER LICENSE -->
+<!-- BSD [![License](https://img.shields.io/badge/license-BSD--3%20clause-blue.svg)](LICENSE) -->
+<!-- Apache2 [![License](https://img.shields.io/badge/license-Apache%202-blue.svg)](LICENSE) -->
+<!-- GPL [![License](https://img.shields.io/badge/license-GPL-blue.svg)](LICENSE) -->
+
+<!-- [![Coverity Scan](https://scan.coverity.com/projects/4884/badge.svg)](https://scan.coverity.com/projects/glfw-glfw) -->
+<!-- [![chat](https://badges.gitter.im/LLGL-Project/LLGL.svg)]() -->
+
+## ■ Documentation
+
+* [Home](#boilerplate-unitypackage)
+* [Documentation](#-documentation)
+* [Introduction](#-introduction)
+* [Build Status](#-build-status)
+* [Install](#-directory-structure)
+  * [from NuGet](#from-nuget)
+  * [from Unity Package Manager](#from-unity-package-manager)
+  * [from unity package](#from-unity-package)
+* [Wiki](#-wiki)
+  * [ScriptReference](#script-reference)
+* [Setup](#-setup)
+* [Dependencies](#-dependencies)
+* [Directory Structure](#-directory-structure)
+* [License](#-license)
+* [Reference](#-reference)
+
+## ■ Introduction
+
+<!-- edit introduction. -->
+
+## ■ Build Status
+
+### *Platform Supports*
+
+<!-- vertical layout -->
+
+| Platform | Test | CI |
+|----------|:--:|:-----:|
+| Editor | [![Build Status](https://img.shields.io/badge/build-passing-brightgreen)](#) | N/A |
+
+## ■ Install
+
+### from NuGet
+
+- nuget package manager.
+
+### from Unity Package Manager
+
+- `manifest.json`
+
+```
+{
+    "com.aria.unity.xxx": "https://github.com/TK-Aria/skeleton.git#master" 
+}
+```
+
+## from unity package
+
+- [download this here Releases](https://github.com/TK-Aria/skeleton/releases)
+
+ <img src="https://pngimage.net/wp-content/uploads/2018/06/unity-logo-white-png-5.png" height="20" /> drag and drop or double click on unity package file.
+
+## ■ Wiki
+
+- [ScriptReference](#)
+
+## ■ Dependencies
+
+The documentation is generated with Doxygen if CMake can find that tool.
+
+## ■ License
+※ [about License](https://qiita.com/tukiyo3/items/58b8b3f51e9dc8e96886)
+
+- [MIT License](./LICENSE)
+
+## ■ Reference
+
+
